7a51b78eddc1315f8fb0da121f138c8f9ecd9512
Irina Bondareva <irina.bondareva1605@gmail.com>
2020-08-10 15:11:47 +0300

    new user registration was added

--- pages/login_page.py
+++ pages/login_page.py
@@ -15,10 +15,10 @@ class LoginPage(BasePage):
         assert "login" in browser.current_url(), "Login is not present in current url"
         
     def should_be_login_form(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login from is not presented"  
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login from is not presented"  
         
     def should_be_register_form(self):
-        assert self.is_element_present(*MainPageLocators.REGISTER_FORM), "Register form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
 
     def register_new_user(self, email, password):
         email_field = self.browser.find_element(*LoginPageLocators.EMAIL)
