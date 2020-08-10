36fe53a7b5acdc3a3415d0d46c32c73e001f5b96
Irina Bondareva <irina.bondareva1605@gmail.com>
2020-08-10 14:39:08 +0300

    stub was added

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 from pages.login_page import ProductPage
 
+
 class MainPage(BasePage):
     def __init__(self, *args, **kwargs):
         super(MainPage, self).__init__(*args, **kwargs)
