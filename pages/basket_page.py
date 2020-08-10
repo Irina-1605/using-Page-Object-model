f5632fc9276f421946342e0065a1ca136fa12c22
Irina Bondareva <irina.bondareva1605@gmail.com>
2020-08-10 15:15:05 +0300

    success message check was added

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -49,4 +49,6 @@ class BasketPage(BasePage):
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is not disappeared, but should be"
 
+
+
               
